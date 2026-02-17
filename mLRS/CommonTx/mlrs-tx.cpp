8df8fddddac6c8440543f54770b5b3d795c64655
JLP <jlpoltrack@gmail.com>
2026-02-17 18:48:10 -0500

    Decode Try

--- mLRS/CommonTx/mlrs-tx.cpp
+++ mLRS/CommonTx/mlrs-tx.cpp
@@ -142,7 +142,7 @@
 #include "../Common/diversity.h"
 #include "../Common/arq.h"
 #include "../Common/tasks.h"
-//#include "../Common/time_stats.h" // un-comment if you want to use
+#include "../Common/time_stats.h" // un-comment if you want to use
 //#include "../Common/test.h" // un-comment if you want to compile for board test
 
 #include "config_id.h"
