9f45025df4cf5393fc2728db7c49c273d139f8c2
MathiyazhaganRJ <mathiyazhaganrj@gmail.com>
2026-02-19 22:29:01 +0530

    Configure Waveshare RP2040 433MHz build environment and device definitions

--- mLRS/Common/hal/rp/rp-device_conf.h
+++ mLRS/Common/hal/rp/rp-device_conf.h
@@ -31,3 +31,31 @@
   #define DEVICE_HAS_SX128x
   #define FREQUENCY_BAND_2P4_GHZ
 #endif
+
+#ifdef TX_WAVESHARE_RP2040
+  #define DEVICE_NAME "Waveshare RP2040 Tx"
+  #define DEVICE_IS_TRANSMITTER
+  #define DEVICE_HAS_SX128x
+  #define FREQUENCY_BAND_2P4_GHZ
+#endif
+
+#ifdef RX_WAVESHARE_RP2040
+  #define DEVICE_NAME "Waveshare RP2040 Rx"
+  #define DEVICE_IS_RECEIVER
+  #define DEVICE_HAS_SX128x
+  #define FREQUENCY_BAND_2P4_GHZ
+#endif
+
+#ifdef TX_WAVESHARE_RP2040_433
+  #define DEVICE_NAME "Waveshare RP2040 433 Tx"
+  #define DEVICE_IS_TRANSMITTER
+  #define DEVICE_HAS_SX126x
+  #define FREQUENCY_BAND_433_MHZ
+#endif
+
+#ifdef RX_WAVESHARE_RP2040_433
+  #define DEVICE_NAME "Waveshare RP2040 433 Rx"
+  #define DEVICE_IS_RECEIVER
+  #define DEVICE_HAS_SX126x
+  #define FREQUENCY_BAND_433_MHZ
+#endif
